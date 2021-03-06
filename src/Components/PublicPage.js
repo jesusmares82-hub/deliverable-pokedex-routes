ff67a6be78e5e517202be1f85f77f29774e73510
Jesus <jesusmares82@gmail.com>
2021-03-06 01:23:34 -0600

    finished

--- src/Components/PublicPage.js
+++ src/Components/PublicPage.js
@@ -89,135 +89,150 @@ const PublicPage = ({ children, ...props }) => {
   };
 
   return (
-    <div className="text-center poke-details font-family">
-      <ReactAudioPlayer src={PokemonSong} autoPlay loop />
+    <>
       <h4 className="text-center mb-4">Welcome Trainer: {user}</h4>
-      <img className="mb-4" src={pokemonlogo} alt="pokemon-logo" />
-      <div>
-        <SearchBox
-          handleSearchTermType={handleSearchType}
-          handleSearchTermName={handleSearchName}
-          handleClearTerm={handleClear}
+
+      <div className="text-center  font-family">
+        <ReactAudioPlayer src={PokemonSong} autoPlay loop />
+        <img
+          className="mb-4 logo-center"
+          src={pokemonlogo}
+          alt="pokemon-logo"
         />
-      </div>
-      {hasData ? (
-        <>
-          {pokes.length > 0 && (
-            <>
-              <ReactPaginate
-                className="color-text-a text-center"
-                previousLabel={"<"}
-                nextLabel={">"}
-                breakLabel={""}
-                breakClassName={"break-me"}
-                pageCount={pageCount}
-                marginPagesDisplayed={0}
-                pageRangeDisplayed={9}
-                onPageChange={handlePageClick}
-                containerClassName={"pagination"}
-                subContainerClassName={"pages pagination"}
-                activeClassName={"active"}
-              />
+        <div>
+          <SearchBox
+            handleSearchTermType={handleSearchType}
+            handleSearchTermName={handleSearchName}
+            handleClearTerm={handleClear}
+          />
+        </div>
+        {hasData ? (
+          <>
+            {pokes.length > 0 && (
+              <>
+                <ReactPaginate
+                  className="color-text-a text-center"
+                  previousLabel={"<"}
+                  nextLabel={">"}
+                  breakLabel={""}
+                  breakClassName={"break-me"}
+                  pageCount={pageCount}
+                  marginPagesDisplayed={0}
+                  pageRangeDisplayed={9}
+                  onPageChange={handlePageClick}
+                  containerClassName={"pagination"}
+                  subContainerClassName={"pages pagination"}
+                  activeClassName={"active"}
+                />
 
-              <div className="pokegallery">{pokes}</div>
-            </>
-          )}
+                <div className="pokegallery">{pokes}</div>
+              </>
+            )}
 
-          {pokemon && (
-            <>
-              <ReactPaginate
-                className="color-text-a text-center"
-                previousLabel={"<"}
-                nextLabel={">"}
-                breakLabel={""}
-                breakClassName={"break-me"}
-                pageCount={pageCount}
-                marginPagesDisplayed={0}
-                pageRangeDisplayed={9}
-                onPageChange={handlePageClick}
-                containerClassName={"pagination"}
-                subContainerClassName={"pages pagination"}
-                activeClassName={"active"}
-              />
-              <div
-                className={
-                  pokemon
-                    ? " my-card normal pt-3 mx-auto show"
-                    : "my-card normal pt-3 hide"
-                }
-              >
-                {pokemon.sprites && (
-                  <img
-                    className="img-container"
-                    width="100px"
-                    src={pokemon.sprites.front_default}
-                    alt={pokemon.name}
-                  />
-                )}
-                {pokemon.name && (
-                  <h5
-                    className="font-family"
-                    style={{
-                      margin: 3,
-                    }}
-                  >
-                    <Link to={`/pokedex/pokemon/${pokemon.id}`}>
-                      {pokemon.name.charAt(0).toUpperCase() +
-                        pokemon.name.slice(1)}
-                    </Link>
-                  </h5>
-                )}
-                <span className="number">#{pokemon.id}</span>
-                {pokemon.types && (
-                  <h6 className="font-family">
-                    <strong>
-                      Type:
-                      {pokemon.types[0].type.name.charAt(0).toUpperCase() +
-                        pokemon.types[0].type.name.slice(1)}{" "}
-                    </strong>
-                  </h6>
-                )}
-                {pokemon.stats && (
-                  <div className="bg-color-white pl-2 pr-2 mt-4">
-                    <h6 className="font-family text-center ">
+            {pokemon && (
+              <>
+                <ReactPaginate
+                  className="color-text-a text-center"
+                  previousLabel={"<"}
+                  nextLabel={">"}
+                  breakLabel={""}
+                  breakClassName={"break-me"}
+                  pageCount={pageCount}
+                  marginPagesDisplayed={0}
+                  pageRangeDisplayed={9}
+                  onPageChange={handlePageClick}
+                  containerClassName={"pagination"}
+                  subContainerClassName={"pages pagination"}
+                  activeClassName={"active"}
+                />
+                <div
+                  className={
+                    pokemon
+                      ? " my-card normal pt-3 mx-auto show"
+                      : "my-card normal pt-3 hide"
+                  }
+                >
+                  {pokemon.sprites && (
+                    <img
+                      className="img-container"
+                      width="100px"
+                      src={pokemon.sprites.front_default}
+                      alt={pokemon.name}
+                    />
+                  )}
+                  {pokemon.name && (
+                    <h5
+                      className="font-family"
+                      style={{
+                        margin: 3,
+                      }}
+                    >
+                      <Link to={`/pokedex/pokemon/${pokemon.id}`}>
+                        {pokemon.name.charAt(0).toUpperCase() +
+                          pokemon.name.slice(1)}
+                      </Link>
+                    </h5>
+                  )}
+                  <span className="number">#{pokemon.id}</span>
+                  {pokemon.types && (
+                    <h6 className="font-family">
                       <strong>
-                        <span>
-                          {" "}
-                          {pokemon.stats[0].stat.name.charAt(0).toUpperCase() +
-                            pokemon.stats[0].stat.name.slice(1)}
-                          : {pokemon.stats[0].base_stat}{" "}
-                        </span>
-                        <p className="p-card">
-                          {" "}
-                          {pokemon.stats[1].stat.name.charAt(0).toUpperCase() +
-                            pokemon.stats[1].stat.name.slice(1)}
-                          : {pokemon.stats[1].base_stat}{" "}
-                        </p>
-                        <span>
-                          {" "}
-                          {pokemon.stats[2].stat.name.charAt(0).toUpperCase() +
-                            pokemon.stats[2].stat.name.slice(1)}
-                          : {pokemon.stats[2].base_stat}{" "}
-                        </span>
-                        <p>
-                          {" "}
-                          {pokemon.stats[5].stat.name.charAt(0).toUpperCase() +
-                            pokemon.stats[5].stat.name.slice(1)}
-                          : {pokemon.stats[5].base_stat}{" "}
-                        </p>
+                        Type:
+                        {pokemon.types[0].type.name.charAt(0).toUpperCase() +
+                          pokemon.types[0].type.name.slice(1)}{" "}
                       </strong>
                     </h6>
-                  </div>
-                )}
-              </div>
-            </>
-          )}
-        </>
-      ) : (
-        <Spinner />
-      )}
-      <h3>{messageError}</h3>
-    </div>
+                  )}
+                  {pokemon.stats && (
+                    <div className="bg-color-white pl-2 pr-2 mt-4">
+                      <h6 className="font-family text-center ">
+                        <strong>
+                          <span>
+                            {" "}
+                            {pokemon.stats[0].stat.name
+                              .charAt(0)
+                              .toUpperCase() +
+                              pokemon.stats[0].stat.name.slice(1)}
+                            : {pokemon.stats[0].base_stat}{" "}
+                          </span>
+                          <p className="p-card">
+                            {" "}
+                            {pokemon.stats[1].stat.name
+                              .charAt(0)
+                              .toUpperCase() +
+                              pokemon.stats[1].stat.name.slice(1)}
+                            : {pokemon.stats[1].base_stat}{" "}
+                          </p>
+                          <span>
+                            {" "}
+                            {pokemon.stats[2].stat.name
+                              .charAt(0)
+                              .toUpperCase() +
+                              pokemon.stats[2].stat.name.slice(1)}
+                            : {pokemon.stats[2].base_stat}{" "}
+                          </span>
+                          <p>
+                            {" "}
+                            {pokemon.stats[5].stat.name
+                              .charAt(0)
+                              .toUpperCase() +
+                              pokemon.stats[5].stat.name.slice(1)}
+                            : {pokemon.stats[5].base_stat}{" "}
+                          </p>
+                        </strong>
+                      </h6>
+                    </div>
+                  )}
+                </div>
+              </>
+            )}
+          </>
+        ) : (
+          <Spinner />
+        )}
+        <h3>{messageError}</h3>
+      </div>
+    </>
   );
 };
 
